c4a0de9916c177634bb0a06b26f03d1b54c1ee0e
achrafrf <itsachrafrafiq@gmail.com>
2025-05-27 16:20:59 +0200

    2eme commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'next-themes';
 import { Footer } from '@/components/footer';
 import { HeroHeader } from '@/components/hero8-header';
 
-
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -15,35 +14,37 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
+// 👇 العنوان الذي يظهر في التبويب
+export const metadata = {
+  title: "DATA FC",
+  description: "وصف اختياري للموقع",
+  icons: {
+  icon: "/favicon.ico",
+},
+};
+
 export default function RootLayout({
   children,
   modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
-  params: { locale: string }; // locale موجود هنا
+  params: { locale: string };
 }>) {
- 
-
   return (
-   <html suppressHydrationWarning>
-  <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-    <ThemeProvider attribute="class">
-      
-      {/* On wrappe le header dans une div fixe */}
-      <div className="fixed top-0 left-0 w-full h-16 z-50">
-        <HeroHeader />
-      </div>
-
-      {/* Décalage du contenu sous le header fixe */}
-      <main className="pt-16">
-        {children}
-      </main>
-
-      {modal}
-      <Footer />
-    </ThemeProvider>
-  </body>
-</html>
+    <html suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute="class">
+          <div className="fixed top-0 left-0 w-full h-16 z-50">
+            <HeroHeader />
+          </div>
+          <main className="pt-16">
+            {children}
+          </main>
+          {modal}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
